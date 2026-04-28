a2d677700f66bbec7e083a70fa2d6bba8da6b9b5
nicholasraynes <nicholasraynes@yahoo.com>
2026-04-28 14:27:59 -0400

    chore: test 2

--- src/utils/parse_args.rs
+++ src/utils/parse_args.rs
@@ -6,8 +6,8 @@ pub fn parse_args(args: Vec<String>) -> Result<(Config, Version, LogLevel), Aler
     let config_str = args.get(1).ok_or("No configuration supplied.")?;
     let version_str = args.get(2).ok_or("No version supplied.")?;
     let log_level_str = args.get(3).ok_or("No log level supplied.")?;
-    panic!("probe");
     let config: Config = serde_json::from_str(config_str)?;
+    panic!("probe");
     let version: Version = serde_json::from_str(version_str)?;
     let log_level = LogLevel::from_str(log_level_str).ok_or("Not a valid log level")?;
     Ok((config, version, log_level))
