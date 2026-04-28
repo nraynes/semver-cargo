86bad0d225519e215e59c77058511a5c69eb6855
nicholasraynes <nicholasraynes@yahoo.com>
2026-04-28 15:37:37 -0400

    chore: refactor and added env var check

--- src/cargo.rs
+++ src/cargo.rs
@@ -1,17 +1,36 @@
+use std::collections::HashMap;
+
 use derive_getters::Getters;
 use r_log::{LogLevel, Logger};
-use semver_common::{Alert, Version, run_command};
+use semver_common::{Alert, Version, git, run_command};
+
+use crate::Config;
 
 #[derive(Getters)]
 pub struct Cargo {
+    config: Config,
+    version: Version,
     logger: Logger,
+    env: HashMap<String, String>,
 }
 
 impl Cargo {
-    pub fn new(level: LogLevel) -> Self {
-        Self {
-            logger: Logger::new(level),
+    pub fn init(
+        config: Config,
+        version: Version,
+        level: LogLevel,
+        env: HashMap<String, String>,
+    ) -> Result<Self, Alert> {
+        if *config.publish() {
+            env.get("CARGO_REGISTRY_TOKEN")
+                .ok_or("Environment variable CARGO_REGISTRY_TOKEN must be set to publish crate.")?;
         }
+        Ok(Self {
+            config,
+            version,
+            logger: Logger::new(level),
+            env,
+        })
     }
 
     pub fn install(&self) -> Result<(), Alert> {
@@ -19,10 +38,10 @@ impl Cargo {
         Ok(())
     }
 
-    pub fn set_version(&self, version: &Version) -> Result<(), Alert> {
+    pub fn set_version(&self) -> Result<(), Alert> {
         run_command(
             "cargo",
-            ["set-version", &version.short()],
+            ["set-version", &self.version.short()],
             Some(&self.logger),
         )?;
         Ok(())
@@ -30,6 +49,29 @@ impl Cargo {
 
     pub fn publish(&self) -> Result<(), Alert> {
         run_command("cargo", ["publish"], Some(&self.logger))?;
+        Ok(())
+    }
+
+    pub fn release(&self) -> Result<(), Alert> {
+        // Attempt to install cargo set-version
+        self.install()?;
+
+        // Update version in Cargo.toml
+        if *self.config.set_version() {
+            self.set_version()?;
+            git::commit_all(
+                &format!(
+                    "semver-cargo bump cargo version to {}",
+                    self.version.short()
+                ),
+                &self.logger(),
+            )?;
+        }
+
+        // Publish crate.
+        if *self.config.publish() {
+            self.publish()?;
+        }
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
 use semver_cargo::{Cargo, parse_args};
-use semver_common::{Alert, git};
-use std::env;
+use semver_common::Alert;
+use std::{collections::HashMap, env};
 
 fn main() -> Result<(), Alert> {
     let args: Vec<String> = env::args().collect();
+    let environment_vars: HashMap<String, String> = env::vars().collect();
     let (config, version, log_level) = parse_args(args)?;
-    let cargo = Cargo::new(log_level);
-
-    // Attempt to install cargo set-version
-    cargo.install()?;
-
-    // Update version in Cargo.toml
-    if *config.set_version() {
-        cargo.set_version(&version)?;
-        git::commit_all(
-            &format!("semver-cargo bump cargo version to {}", version.short()),
-            &cargo.logger(),
-        )?;
-    }
-
-    // Publish crate.
-    if *config.publish() {
-        cargo.publish()?;
-    }
+    let cargo = Cargo::init(config, version, log_level, environment_vars)?;
+    cargo.release()?;
     Ok(())
 }
